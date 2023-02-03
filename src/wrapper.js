5bce4903d0077dc0af3a08c7109136f58c27b2ee
Calum Chamberlain <calum@appoly.co.uk>
2023-02-03 13:42:44 +0000

    vue-3 & vite

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,28 +1,3 @@
-// Import vue component
-import component from './components/VueModal.vue';
+import VueModal from './components/VueModal.vue'
 
-// Declare install function executed by Vue.use()
-export function install(Vue) {
-	if (install.installed) return;
-	install.installed = true;
-	Vue.component('VueModal', component);
-}
-
-// Create module definition for Vue.use()
-const plugin = {
-	install,
-};
-
-// Auto-install when vue is found (eg. in browser via <script> tag)
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-	GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-	GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-	GlobalVue.use(plugin);
-}
-
-// To allow use as module (npm/webpack/etc.) export component
-export default component;
+export { VueModal };
